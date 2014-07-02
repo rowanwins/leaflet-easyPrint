7338f2161c9386fd1fdf9139426e208f059d8ccb
rowanwinsemius <rowan.winsemius@lls.nsw.gov.au>
2014-07-02 21:53:49 +1000

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-leaflet-easyPrint
-=================
+##leaflet-easyPrint
+
 
 A leaflet plugin which adds an icon to print the map
+
+### Usage
+Step 1. Include the required js and css files  
+
+Step 2. Add the following line of code to your map script
+
+``` js
+L.easyPrint().addTo(map)
+```
+
+Step 3. Sit back and relax!
---
+++ dist/leaflet.easyPrint.js
@@ -0,0 +1,25 @@
+L.Control.EasyPrint = L.Control.extend({
+    options: {
+        position: 'topleft',
+        title: 'Print map',
+    },
+
+    onAdd: function () {
+        var container = L.DomUtil.create('div', 'leaflet-control-easyPrint leaflet-bar leaflet-control');
+
+        this.link = L.DomUtil.create('a', 'leaflet-control-easyPrint-button leaflet-bar-part', container);
+        this.link.href = 'javascript:window.print()';
+
+        return container;
+    },
+    
+  
+    _click: function (e) {
+        L.DomEvent.stopPropagation(e);
+        L.DomEvent.preventDefault(e);
+    },
+});
+
+L.easyPrint = function(options) {
+  return new L.Control.EasyPrint(options);
+};
